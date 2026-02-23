f3a8c3625980bb6230d18856af307ede6a351aab
Xavier <xavieralves162@gmail.com>
2026-02-23 10:42:24 +0000

    Exc 8.5

--- library-backend.js
+++ library-backend.js
@@ -114,7 +114,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -125,11 +125,21 @@ const resolvers = {
     authorCount: () => authors.length,
 
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books
+      let filteredBooks = books
+
+      if (args.author) {
+        filteredBooks = filteredBooks.filter(
+          book => book.author === args.author
+        )
       }
 
-      return books.filter(book => book.author === args.author)
+      if (args.genre) {
+        filteredBooks = filteredBooks.filter(
+          book => book.genres.includes(args.genre)
+        )
+      }
+
+      return filteredBooks
     },
 
     allAuthors: () => authors,
